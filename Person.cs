328a6cd11dc7126d4d1ba55b05b22c03a9148488
Vladym1r0FF <vkalpakchiew@gmail.com>
2019-04-21 12:29:30 +0300

    New Updates Has Been Added

---
+++ Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salary
+{
+    class Person
+    {
+        private string firstName;
+        public string FirstName
+        {
+            get { return this.firstName; }
+        }
+
+        private string lastName;
+
+        private int age;
+        public int Age
+        {
+            get { return this.age; }
+        }
+        private double salary;
+        public double Salary
+        {
+            get { return this.salary; }
+        }
+        public void IncreaseSalary(double bonus)
+        {
+            double multiplyer = 1.0 + (bonus / 100);
+            this.salary *= multiplyer;
+        }
+        public Person(string name, string family, int age, double salary)
+        {
+            this.firstName = name;
+            this.lastName = family;
+            this.age = age;
+            this.salary = salary;
+        }
+        public override string ToString()
+        {
+            return $"{firstName} {this.lastName} get {this.salary:f2} leva";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var persons = new List<Person>();
+            var lines = int.Parse(Console.ReadLine());
+            for (int i = 0; i < lines; i++)
+            {
+                var cmdArgs = Console.ReadLine().Split();
+                var person = new Person(cmdArgs[0], cmdArgs[1], int.Parse(cmdArgs[2]), double.Parse(cmdArgs[3]));
+
+                persons.Add(person);
+            }
+            var bonus = double.Parse(Console.ReadLine());
+            persons.ForEach(p => p.IncreaseSalary(bonus));
+
+            persons.ForEach(p => Console.WriteLine(p.ToString())); 
+        }
+    }
+}
